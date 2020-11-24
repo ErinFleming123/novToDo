7b9b76f57932d9c0a9c2f21d01edb5bd1e9a8fc9
Erin Fleming <gronkerin@gmail.com>
2020-11-23 20:54:08 -0700

    nov23/2020first

--- novToDo.js
+++ novToDo.js
@@ -1,83 +1,30 @@
-window.onload = init 
-
-function init(){
-
-
-let button2 = document.getElementById("addListNameButton")
-button2.onclick = handleButtonClick2
-
+window.onload = init
+function init() {
+  let button1 = document.getElementById("addNewListButton")
+  button1.onclick = handleButtonClick1
 }
 
-
-
-//This is for the button that adds the list name
-function handleButtonClick2() {
-  let listNameInput = document.getElementById("listName")
-  let listName = listNameInput.value
-  
-  if (listName !=="") {
-    alert("Adding " + listName)
-    
-    
-
-
-let p = document.createElement("p")
-p.innerHTML = listName
-
-let h1 = document.getElementById("taskListTitle")
-
-h1.appendChild(p)
-p.insertAdjacentHTML(
-  "beforeend", "<button>Delete List</button></br><form><input type='text' size='40' placeholder='Write a new task here'><button>Add Task</button></form></br><ul></ul>"
-  )
-
-
-}else {
-  alert("Please add a list name.")
- 
-}
-
-}
-//This is for the button that adds tasks
 function handleButtonClick1() {
-  let textInput = document.getElementById("taskInput")
-  let task = textInput.value
+  let newListInput = document.getElementById("listName")
+  let newListName = newListInput.value
+  if (newListName !== "") {
+    alert("Adding " + newListName)
+    let p = document.createElement("p")
+    p.innerHTML = newListName
+    let h2 = document.getElementById("taskListTitle")
+    h2.appendChild(p)
+    let delListBtn = document.createElement("button")
+    delListBtn.innerHTML = "Delete List"
+    p.appendChild(delListBtn)
+
+    newListInput.value = ""
+    
+  }else{
+    alert("Please enter a name for a new list.")
+  }
   
-  if (task =="") {
-    alert("Please add a task")
-}else {
-  alert("Adding " + task)
-}
-
-let li = document.createElement("li")
-//li.innerHTML = task + `<button>Delete</button>`
-
-
-let ul = document.getElementById("taskList")
-ul.appendChild(li)
-li.innerHTML = task
-
-li.insertAdjacentHTML("beforeend", "<button onclick='handleDelete(this)'>Delete</button>")
-
-
-
-}
-
-//This is for the delete button that deletes the task.
-
-function handleDelete(e) {
-  e.parentNode.parentNode.removeChild(e.parentNode)
-
+  
   
 
-
+  
 }
-
-
-
-
-
-
-
-
-
