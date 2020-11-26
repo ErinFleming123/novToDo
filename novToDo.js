601da2affa43f9d7c52831d15ae1423340e9c07e
Erin Fleming <gronkerin@gmail.com>
2020-11-25 19:01:29 -0700

    nov25/2020/first

--- novToDo.js
+++ novToDo.js
@@ -2,7 +2,24 @@ window.onload = init
 function init() {
   let button1 = document.getElementById("addNewListButton")
   button1.onclick = handleButtonClick1
+  let button2 = document.getElementByClass("addTaskBtn")
+  button2.onclick = handleButtonClick2
+  
+  
 }
+let button2 = document.getElementByClass("addTaskBtn")
+  button2.onclick = handleButtonClick2
+  function handleButtonClick2() {
+    let newTaskInput = document.getElementsByClassName("taskName")
+    let newTaskName = newTaskInput.value
+    if (newTaskName !== "") {
+      alert("Adding " + newTaskName)
+      let li = document.createElement("LI")
+      li.innerHTML = newTaskName
+      ul.appendChild(li)
+    }
+
+  }
 
 function handleButtonClick1() {
   let newListInput = document.getElementById("listName")
@@ -17,16 +34,17 @@ function handleButtonClick1() {
     delListBtn.innerHTML = "Delete List"
     p.appendChild(delListBtn)
     let taskForm = document.createElement("FORM")
-    taskForm.innerHTML = `<input type="text" size="40" placeholder="Write new task here">
-    <input type="button" value="Add New Task">`
+    taskForm.innerHTML = `<input type="text"size="40" class="taskName" placeholder="Write new task here">
+    <input type="button" class="addTaskBtn" value="Add New Task">`
     p.appendChild(taskForm)
-    
-    
+    let ul = document.createElement("UL")
+    taskForm.appendChild(ul)
     newListInput.value = ""
     
   }else{
     alert("Please enter a name for a new list.")
   }
+  
   
   
   
